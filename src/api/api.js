ef628410c76868957d868f8be51cca69b6ba2302
Dmitry Afonasenko <Dmitrytbp@gmail.com>
2023-08-28 17:51:17 +0300

    Добавил обращение к api

---
+++ src/api/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_URL = "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=JdZrMyQiNT5Zg1vwjiY2H2UCEWw8K7ZnTQdm8rww";
+
+export const getAsteroids = async () => {
+  try {
+    const result = await axios.get(API_URL);
+    console.log("🚀 ~ file: api.js:8 ~ getAsteroids ~ result:", result)
+    
+  } catch ( {message} ) {
+    // this.$log.error(message)
+  }
+}
---
+++ src/utils/utils.js
@@ -0,0 +1,5 @@
+export const convertApiData = (data = []) => {
+  if (!data.length) {
+    return [];
+  }
+}
