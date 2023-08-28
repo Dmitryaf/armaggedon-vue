2e9315d1e1cb113d48f0b5e843284e75f95c9e1d
Dmitry Afonasenko <Dmitrytbp@gmail.com>
2023-08-28 18:00:46 +0300

    Вынес api key в константу

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=JdZrMyQiNT5Zg1vwjiY2H2UCEWw8K7ZnTQdm8rww";
+const API_KEY = 'JdZrMyQiNT5Zg1vwjiY2H2UCEWw8K7ZnTQdm8rww';
+const API_URL = `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${API_KEY}`;
 
 export const getAsteroids = async () => {
   try {
