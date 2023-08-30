b72a6b0a9b1a82bee2e6028977b42604a73cabb2
Dmitry Afonasenko <Dmitrytbp@gmail.com>
2023-08-30 09:00:32 +0300

    Добавил методы convertApiData и covertDateFormat
    Добавил метод для конвертации данных api, чтобы привести данные получаемые с api в более читаемый вид

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,40 @@
-export const convertApiData = (data = []) => {
-  if (!data.length) {
+export const convertApiData = (data) => {
+  if (!data) {
     return [];
   }
+  let result = []
+  for (const dataTtem in data) {
+    const convertedData = data[dataTtem].map((item) => {
+      return {
+        name: item.name,
+        estimatedDiameter:
+          item.estimated_diameter.meters.estimated_diameter_max,
+        closeApproachDate: item.close_approach_data[0].close_approach_date,
+        missDistance: {
+          kilometers: item.close_approach_data[0].miss_distance.kilometers,
+          lunar: item.close_approach_data[0].miss_distance.lunar,
+        },
+        isPotentiallHazardous: item.is_potentially_hazardous_asteroid,
+      }
+    })
+    result = result.concat(convertedData)
+  }
+  result.sort((a,b) => {
+    const firstDate = new Date(a.closeApproachDate).getTime();
+    const secondDate = new Date(b.closeApproachDate).getTime();
+    return firstDate - secondDate;
+  })
+  return result;
+}
+
+export const covertDateFormat = (date, separator) => {
+  if (!date) {
+    return null
+  }
+  const newDate = new Date(date)
+  const year = newDate.getFullYear()
+  const month = ('0' + (newDate.getMonth() + 1)).slice(-2)
+  const day = ('0' + newDate.getDate()).slice(-2)
+
+  return `${year}${separator}${month}${separator}${day}`
 }
