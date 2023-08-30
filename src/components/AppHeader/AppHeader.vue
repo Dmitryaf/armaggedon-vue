93403b16472d6b53b590626233658f79f78b4373
Dmitry Afonasenko <Dmitrytbp@gmail.com>
2023-08-30 15:25:29 +0300

    Добавил компоненты навигации и шапки

---
+++ src/components/AppHeader/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header">
+    <div class="header__info">
+      <h1 class="header__title">ARMAGEDDON 2023</h1>
+      <p class="header__phrase">ООО “Команда им. Б. Уиллиса”.</p>
+      <p class="header__phrase">Взрываем астероиды с 1998 года.</p>
+    </div>
+    <div class="header__nav">
+      <app-navigation></app-navigation>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import AppNavigation from '@/components/AppNavigation/AppNavigation.vue';
+</script>
+
+<style src="./AppHeader.css" scoped></style>
---
+++ src/components/AppNavigation/AppNavigation.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar">
+    <ul class="navbar__nav">
+      <li v-for="link of routes" :key="link.name" class="navbar__item">
+        <app-link
+          class="navbar__link"
+          active-class="active"
+          :name="link.name"
+          :path="link.path"
+        >
+          {{ link.name }}
+        </app-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+import { routes } from '@/router/index';
+
+import AppLink from '@/components/AppLink.vue';
+</script>
+
+<style src="./AppNavigation.css" scoped></style>
